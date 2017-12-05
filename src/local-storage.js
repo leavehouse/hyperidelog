69e087986516a311a8eda9eab66d0c1a952e1a89
leavehouse <heavelouse@gmail.com>
2017-12-05 00:14:40 -0600

    Give credit where credit is due

--- src/local-storage.js
+++ src/local-storage.js
@@ -1,5 +1,8 @@
 const STORAGE_KEY = 'ridelog/v0';
 
+// Adapted slightly from
+// https://github.com/marcusasplund/hyperapp-todo-simple/blob/master/src/utils/local-storage.js
+
 function retrieveData() {
   return JSON.parse(window.localStorage.getItem(STORAGE_KEY));
 }
